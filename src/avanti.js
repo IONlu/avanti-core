634b797e7f1936a84337d1f6cd0defd3599a7e85
Daniel Duton <daniel@duton.lu>
2016-06-03 16:28:53 +0200

    connect to avanti manager

--- src/avanti.js
+++ src/avanti.js
@@ -1,2 +1,14 @@
-var Pool = require('./pool.js'),
-    Customer = require('./customer.js');
+const io = require('socket.io-client');
+const commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [{
+    name: 'host',
+    alias: 'h',
+    type: String
+}];
+const options = commandLineArgs(optionDefinitions);
+
+var socket = io.connect(options.host || 'http://localhost:3000');
+socket.on('connect', () => console.log('connect'));
+
+console.log('Connecting to: ' + options.host || 'http://localhost:3000');
