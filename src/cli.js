50eb60811e2c5eda6ae1267440bf4de2aac12f4b
Daniel Duton <daniel@duton.lu>
2016-09-30 17:04:13 +0200

    add cli for host

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import commander from 'commander';
 import * as Customer from './cli/customer.js';
+import * as Host from './cli/host.js';
 
 commander
     .version('0.1.0');
@@ -8,6 +9,12 @@ commander
     .command('customer <action> <customer>')
     .action(function(action, customer) {
         Customer[action](customer);
+    });
+
+commander
+    .command('host <action> <customer> <hostname>')
+    .action(function(action, customer, hostname) {
+        Host[action](customer, hostname);
     });
 
 commander
---
+++ src/cli/host.js
@@ -0,0 +1,19 @@
+import Customer from '../../src/customer.js';
+
+const create = async (customer, hostname) => {
+    try {
+        await (new Customer(customer)).addHost(hostname);
+    } catch(e) {
+        console.error(e);
+    }
+};
+
+const remove = async (customer, hostname) => {
+    try {
+        await (new Customer(customer)).removeHost(hostname);
+    } catch(e) {
+        console.error(e);
+    }
+};
+
+export { create, remove };
