e6cafa51dbe58b86b406bc0907484b8137914237
Daniel Duton <daniel@duton.lu>
2017-02-25 11:37:58 +0100

    use process.stdout instead of console.log

--- src/cli/client.js
+++ src/cli/client.js
@@ -25,11 +25,11 @@ const list = async () => {
         var clients = await Client.all();
         var clientLength = clients.reduce((a, b) => Math.max(a, b.client.length), 0);
         var userLength   = clients.reduce((a, b) => Math.max(a, b.user.length), 0);
-        console.log(clients.map(client => {
+        process.stdout.write(clients.map(client => {
             return pad(client.client, clientLength)
                 + '  ' + pad(client.user, userLength)
                 + '  ' + client.path;
-        }).join('\n'));
+        }).join('\n') + '\n');
     } catch(e) {
         process.exitCode = 1;
         process.stderr.write(chalk.red(chalk.bold('ERROR:') + ' ' + e) + '\n');
