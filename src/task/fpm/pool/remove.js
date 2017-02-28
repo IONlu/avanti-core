fdc8c4fdefd5c25becf73782cf0a7f3e7c95c6aa
Daniel Duton <daniel@duton.lu>
2017-02-28 17:18:47 +0100

    show warning when removing pool file which does not exist

--- src/task/fpm/pool/remove.js
+++ src/task/fpm/pool/remove.js
@@ -1,7 +1,15 @@
 import Promise from 'bluebird';
 import fs from 'fs';
 const unlink = Promise.promisify(fs.unlink);
+import { Warning } from '../../../task';
 
 export const run = async ({ hostname }) => {
-    return await unlink(`/etc/php/7.0/fpm/pool.d/${hostname}.conf`);
+    try {
+        return await unlink(`/etc/php/7.0/fpm/pool.d/${hostname}.conf`);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw new Warning(err.message);
+        }
+        throw err;
+    }
 };
