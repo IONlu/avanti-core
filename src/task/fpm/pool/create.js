ac3a54953ad4fadc3944fb9511ca9862a84d6df4
Daniel Duton <daniel@duton.lu>
2017-05-31 14:08:31 +0200

    remove old fpm pool files

--- src/task/fpm/pool/create.js
+++ src/task/fpm/pool/create.js
@@ -1,7 +1,10 @@
 import Promise from 'bluebird';
 import fs from 'fs';
+import * as Task from '../../../task';
+
 const writeFile = Promise.promisify(fs.writeFile);
 
 export const run = async ({ hostname, php, data }) => {
+    await Task.run('fpm.pool.remove', { hostname });
     return writeFile(`/etc/php/${php}/fpm/pool.d/${hostname}.conf`, data);
 };
