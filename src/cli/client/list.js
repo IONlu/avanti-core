9dc6ca9cafa27c81c72b2c4ccf655fd6353d53f0
Daniel Duton <daniel@duton.lu>
2017-06-01 14:07:03 +0200

    use ascii tables

--- src/cli/client/list.js
+++ src/cli/client/list.js
@@ -1,17 +1,21 @@
 import Client from '../../client.js';
 import chalk from 'chalk';
-import pad from 'pad';
+import { table } from 'table';
 
 export const execute = async () => {
     try {
         var clients = await Client.all();
-        var clientLength = clients.reduce((a, b) => Math.max(a, b.client.length), 0);
-        var userLength   = clients.reduce((a, b) => Math.max(a, b.user.length), 0);
-        process.stdout.write(clients.map(client => {
-            return pad(client.client, clientLength)
-                + '  ' + pad(client.user, userLength)
-                + '  ' + client.path;
-        }).join('\n') + '\n');
+        var rows = clients.map(row => {
+            return [
+                row.client,
+                row.user,
+                row.path
+            ];
+        });
+        var header = [
+            'Client', 'User', 'Path'
+        ].map(text => chalk.bold(text));
+        process.stdout.write(table([ header, ...rows ]));
     } catch(e) {
         process.exitCode = 1;
         process.stderr.write(chalk.red(chalk.bold('ERROR:') + ' ' + e) + '\n');
--- src/cli/host/list.js
+++ src/cli/host/list.js
@@ -1,19 +1,23 @@
 import Host from '../../host.js';
 import chalk from 'chalk';
-import pad from 'pad';
+import { table } from 'table';
 
 export const execute = async () => {
     try {
         var hosts = await Host.all();
-        var hostLength = hosts.reduce((a, b) => Math.max(a, b.host.length), 0);
-        var userLength = hosts.reduce((a, b) => Math.max(a, b.user.length), 0);
-        var pathLength = hosts.reduce((a, b) => Math.max(a, b.path.length), 0);
-        process.stdout.write(hosts.map(host => {
-            return pad(host.host, hostLength)
-                + '  ' + pad(host.user, userLength)
-                + '  ' + pad(host.path, pathLength)
-                + '  ' + host.client;
-        }).join('\n') + '\n');
+        var rows = hosts.map(row => {
+            return [
+                row.host,
+                row.user,
+                row.path,
+                row.client,
+                row.php
+            ];
+        });
+        var header = [
+            'Host', 'User', 'Path', 'Client', 'PHP'
+        ].map(text => chalk.bold(text));
+        process.stdout.write(table([ header, ...rows ]));
     } catch(e) {
         process.exitCode = 1;
         process.stderr.write(chalk.red(chalk.bold('ERROR:') + ' ' + e) + '\n');
