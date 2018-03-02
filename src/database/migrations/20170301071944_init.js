f524b9225fc6c135d611d0ae9275ff1947ba1ea4
Daniel Duton <daniel@duton.lu>
2018-03-02 14:36:21 +0100

    update packages and update migrations

--- src/database/migrations/20170301071944_init.js
+++ src/database/migrations/20170301071944_init.js
@@ -1,15 +1,23 @@
 exports.up = function(knex, Promise) {
     return Promise.all([
-        knex.schema.createTableIfNotExists('client', function(table) {
-            table.string('client', 100).notNullable().primary();
-            table.string('user', 100).notNullable();
-            table.string('path', 200).notNullable();
+        knex.schema.hasTable('client').then(function(exists) {
+            if (!exists) {
+                return knex.schema.createTable('client', function(table) {
+                    table.string('client', 100).notNullable().primary();
+                    table.string('user', 100).notNullable();
+                    table.string('path', 200).notNullable();
+                })
+            }
         }),
-        knex.schema.createTableIfNotExists('host', function(table) {
-            table.string('host', 100).notNullable().primary();
-            table.string('client', 100).notNullable();
-            table.string('user', 100).notNullable();
-            table.string('path', 200).notNullable();
+        knex.schema.hasTable('host').then(function(exists) {
+            if (!exists) {
+                return knex.schema.createTable('host', function(table) {
+                    table.string('host', 100).notNullable().primary();
+                    table.string('client', 100).notNullable();
+                    table.string('user', 100).notNullable();
+                    table.string('path', 200).notNullable();
+                })
+            }
         })
     ]);
 };
