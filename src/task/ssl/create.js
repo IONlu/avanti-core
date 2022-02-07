561833007972cb9e110899f1bd988a4dea984073
jose <jose@ion.lu>
2022-02-07 15:46:39 +0100

    create symlink for file instad of copying file in order to make certbot renewal work

--- src/task/ssl/create.js
+++ src/task/ssl/create.js
@@ -1,21 +1,31 @@
 import Promise from 'bluebird';
-import { create, exists } from '../../utils/file'
+import {
+    create,
+    exists
+} from '../../utils/file'
 import fs from 'fs';
 import exec from '../../exec.js';
 
 const readFile = Promise.promisify(fs.readFile);
 
-export const run = async ({ host, path, method }) => {
+export const run = async ({
+    host,
+    path,
+    method,
+    hostAndAliases
+}) => {
     if (method === 'apache') {
         try {
-            await exec(`letsencrypt certonly -d ${host} --apache --quiet`);
+            await exec(`letsencrypt certonly -d ${hostAndAliases} --apache --quiet`);
         } catch (err) {
             throw new Error('Failed to create certificates using Apache Method')
         }
     } else if (method === 'dns') {
         try {
-            let hookScript =__dirname + '/../../scripts/dnsAcmeHook.js'
-            await exec(`certbot certonly --manual-public-ip-logging-ok --preferred-challenge=dns  --manual-auth-hook ${hookScript} --cert-name ${host} -d ${host} --manual --quiet`, null, null, { ...process.env });
+            let hookScript = __dirname + '/../../scripts/dnsAcmeHook.js'
+            await exec(`certbot certonly --manual-public-ip-logging-ok --preferred-challenge=dns  --manual-auth-hook ${hookScript} -d ${hostAndAliases} --manual --quiet`, null, null, {
+                ...process.env
+            });
         } catch (err) {
             throw new Error('Failed to create certificates using DNS Method')
         }
@@ -26,19 +36,29 @@ export const run = async ({ host, path, method }) => {
     if (letsEncryptCertsExist.includes(false)) {
         throw new Error('CertBot Certs not created')
     } else {
-        let fullchain = await readFile(`/etc/letsencrypt/live/${lastmodifiedFolder}/fullchain.pem`)
-        let privkey = await readFile(`/etc/letsencrypt/live/${lastmodifiedFolder}/privkey.pem`)
-        return Promise.all([create(path + '/certs/fullchain.pem', fullchain), create(path + '/certs/privkey.pem', privkey)])
+        let fullchain = await path.resolve(`/etc/letsencrypt/live/${lastmodifiedFolder}/fullchain.pem`)
+        let privkey = await path.resolve(`/etc/letsencrypt/live/${lastmodifiedFolder}/privkey.pem`)
+        return Promise.all(
+            [
+                exec('ln -s {{source}} {{destination}}', {
+                    source: privkey,
+                    destination: path.resolve(path + '/certs/privkey.pem')
+                }),
+                exec('ln -s {{source}} {{destination}}', {
+                    source: fullchain,
+                    destination: path.resolve(path + '/certs/fullchain.pem')
+                }),
+            ])
     }
 };
 
-async function getlastModifiedFolder (domain) {
+async function getlastModifiedFolder(domain) {
     let path = '/etc/letsencrypt/live'
-    return fs.readdirSync(path).filter(function (file) {
-        return file.startsWith(domain) && fs.statSync(path+'/'+file).isDirectory()
+    return fs.readdirSync(path).filter(function(file) {
+        return file.startsWith(domain) && fs.statSync(path + '/' + file).isDirectory()
     }).reduce((last, current) => {
-        let currentFileDate = new Date(fs.statSync(path+'/' + current).mtime);
-        let lastFileDate = new Date(fs.statSync(path+'/' + last).mtime);
-        return ( currentFileDate.getTime() > lastFileDate.getTime() ) ? current: last;
+        let currentFileDate = new Date(fs.statSync(path + '/' + current).mtime);
+        let lastFileDate = new Date(fs.statSync(path + '/' + last).mtime);
+        return (currentFileDate.getTime() > lastFileDate.getTime()) ? current : last;
     })
 }
